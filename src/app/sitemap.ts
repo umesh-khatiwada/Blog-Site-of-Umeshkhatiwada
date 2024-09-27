271b828b2a4eaf5d64a5a73624ebc0ebffe3af27
Umesh Khatiwada <umesh.khatiwada143@gmail.com>
2024-09-27 23:53:57 +0545

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,10 @@
+
+
 import { MetadataRoute } from 'next'
 import { BASE_URL } from './types/contants'
 import { Article } from './types/blog'
 
-async function fetchPosts(): Promise<Article> {
+async function fetchPosts(): Promise<{ data: Article[] }> {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}blogs?fields[0]=slug&fields[1]=publishedAt&populate[img][fields][0]=url`
   )
@@ -14,40 +16,39 @@ async function fetchPosts(): Promise<Article> {
 
 // Main sitemap generation function
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const apiResponse = await fetchPosts()
-  const posts = apiResponse.data
-
-  const baseUrl = BASE_URL || 'http://localhost:3000'
+  const apiResponse = await fetchPosts();
+  const posts = apiResponse.data;
+  const baseUrl = BASE_URL || 'http://localhost:3000';
 
   // Generate entries for blog posts
-  const postEntries: MetadataRoute.Sitemap = posts.map((post: Article) => {
-    const lastModified = post.publishedAt ? new Date(post.publishedAt) : new Date();
-  
-    return {
-      url: `${baseUrl}/article/${post.attributes.slug}`,
-      lastModified: lastModified.toISOString(),
-      changeFrequency: 'daily',
-      priority: 0.7,
-    }
-  })
-  
+  const postEntries: MetadataRoute.Sitemap = posts
+    .filter(post => post.attributes.slug) // Ensure that only posts with a slug are processed
+    .map(post => {
+      const lastModified = post.attributes.publishedAt ? new Date(post.attributes.publishedAt) : new Date();
+      return {
+        url: `${baseUrl}/article/${post.attributes.slug}`,
+        lastModified: lastModified.toISOString(),
+        changeFrequency: 'daily',
+        priority: 0.7,
+      }
+    });
 
   // Static entries for main pages
   const staticEntries: MetadataRoute.Sitemap = [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: `${baseUrl}/article/[slug]`,
-      lastModified: new Date(),
+      lastModified: new Date().toISOString(),
       changeFrequency: 'daily',
       priority: 0.8,
     },
-  ]
+  ];
 
   // Combine static and dynamic entries
-  return [...staticEntries, ...postEntries]
+  return [...staticEntries, ...postEntries];
 }
