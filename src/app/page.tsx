d33704c6c2fc59e126ba1815f93c0d55bcbf0ba4
Umesh Khatiwada <umesh.khatiwada143@gmail.com>
2024-10-17 14:20:31 +0545

    canonocal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `Umesh Khatiwada - DevOps Professional & Cloud Architect`,
+    description: "Explore Umesh Khatiwada's expertise in cloud infrastructure, automation, and software development. DevOps Professional & Cloud Architect offering services and insights.",
+  }
+}
 
 export default function Home() {
 
+  
   return (
     <div
       className="min-h-screen flex flex-col items-center justify-between text-white"
